b7484702693b069fbf937d8e8963497878842e48
ParthNarkhede <125216216+ParthNarkhede@users.noreply.github.com>
2024-08-12 11:30:42 +0000

    revised some prev learnt

---
+++ iterations.js
@@ -0,0 +1,27 @@
+// const str="   400"
+// const num=400
+// console.log(typeof (num + typeof num));
+// console.log(str);
+
+// console.log(str.trim());
+
+// const strnu="parthparthnarkh"
+// const nestr=strnu.replaceAll("parth","vedu")
+// console.log(nestr);
+
+// const numb=num.toFixed(2)
+// const nwans=Number(numb);
+// console.log(nwans.toFixed(2),num);
+
+// console.log(typeof nwans.toFixed(2));
+
+
+// const dig=678
+// console.log(dig.toPrecision(2));
+
+
+
+
+
+
+
