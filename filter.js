4d604eacc68d81ca5402a9dff749991fe37b42ee
ParthNarkhede <125216216+ParthNarkhede@users.noreply.github.com>
2024-08-12 12:37:03 +0000

    learned about filter map and reduce

---
+++ filter.js
@@ -0,0 +1,16 @@
+const arr=[1,2,3,4,5,6,7,8,9];
+
+const newfiltered=arr.filter((num)=>num>3);
+console.log(newfiltered);
+
+const newmapp=arr.map((nums)=>nums>5)
+console.log(newmapp);
+
+
+const myarr=[1,2,3,4]
+
+const redarr=myarr.reduce((acc,curr)=>acc+curr,-10)
+console.log(redarr);
+
+
+
