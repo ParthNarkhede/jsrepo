f627490e0642276515c846685962fc7a1df5ad19
ParthNarkhede <125216216+ParthNarkhede@users.noreply.github.com>
2024-08-13 09:24:57 +0000

    revised filter reduce map on a random array

--- array.js
+++ array.js
@@ -5,3 +5,17 @@ newarr.unshift(10)
 console.log(arr);
 
 
+const myar=[9,8,7,6,5,4,3,2,1]
+const filtered=myar.filter(function(num){
+    return num>5;
+})
+console.log(filtered);
+
+const mapped=myar.map((num)=>(num*10-27))
+console.log(mapped);
+console.log(myar);
+
+
+
+
+
